c249c2e01259469fc30b8da323fe8f24a135dcad
rupesh9999 <rupesh.b500@gmail.com>
2025-03-28 13:03:32 +0000

    created_rivers_exercise

---
+++ looping_through_dictionary/rivers.py
@@ -0,0 +1,17 @@
+rivers = {
+    'nile': 'egypt',
+    'mississippi': 'united states',
+    'fraser': 'canada',
+    'kuskokwim': 'alaska',
+}
+
+for river, country in rivers.items():
+    print(f"The {river.title()} runs through {country.title()}.")
+
+print("\nThe following rivers are included in this data set:")
+for river in rivers.keys():
+    print(f"- {river.title()}")
+
+print("\nThe following countries are included in this data set:")
+for country in rivers.values():
+    print(f"- {country.title()}")
