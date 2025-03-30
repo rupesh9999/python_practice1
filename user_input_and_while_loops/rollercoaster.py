15a3f8eb3dd0cce8e17a16bbee348963e405a9f0
rupesh9999 <rupesh.b500@gmail.com>
2025-03-30 12:31:34 +0000

    roller_coaster

---
+++ user_input_and_while_loops/rollercoaster.py
@@ -0,0 +1,8 @@
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 48:
+    print(f"\nYou're tall enough to ride!")
+else:
+    print(f"\nYou'll be able to ride when you're a little older.")
+# This program checks if a user is tall enough to ride a roller coaster.
