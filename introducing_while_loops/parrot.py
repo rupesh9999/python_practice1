7d1328bbee5e877450b0b6bcd5148d2a8f6bc8d9
rupesh9999 <rupesh.b500@gmail.com>
2025-03-30 14:46:38 +0000

    modified

--- introducing_while_loops/parrot.py
+++ introducing_while_loops/parrot.py
@@ -1,6 +1,9 @@
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program."
+
 message = ""
 while message != 'quit':
     message = input(prompt)
-    print(message)
+
+    if message != 'quit':
+        print(message)
