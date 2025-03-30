7757c82335952e0bd0f49d6cf8f458ce8849e586
rupesh9999 <rupesh.b500@gmail.com>
2025-03-30 12:37:23 +0000

    commited

---
+++ user_input_and_while_loops/even_or_odd.py
@@ -0,0 +1,7 @@
+number = input("Enter a number, and I will tell you if it is even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print(f"\n{number} is even.")
+else:
+    print(f"\n{number} is odd.")
+# This program checks if a number is even or odd.
