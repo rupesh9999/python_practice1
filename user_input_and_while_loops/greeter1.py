6152205839caee3dee51ec48dca3ab2747f700a1
rupesh9999 <rupesh.b500@gmail.com>
2025-03-30 12:21:50 +0000

    commited

---
+++ user_input_and_while_loops/greeter1.py
@@ -0,0 +1,5 @@
+prompt = "If you tell us who you are, we can personalize the message you see in the morning."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
