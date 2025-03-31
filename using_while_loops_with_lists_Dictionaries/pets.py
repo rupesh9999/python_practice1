ebf9367009de1ce99c95b044992cfda016a07956
rupesh9999 <rupesh.b500@gmail.com>
2025-03-31 14:01:31 +0000

    created_pets

---
+++ using_while_loops_with_lists_Dictionaries/pets.py
@@ -0,0 +1,6 @@
+pets = ['dog', 'cat', 'parrot', 'hamster', 'fish', 'cat', 'goldfish', 'rabbit']
+print(pets)
+# Remove all instances of 'cat' from pets list.
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
