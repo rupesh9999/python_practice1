d578fd545788425a6d12f9392cb015a610f69d77
rupesh9999 <rupesh.b500@gmail.com>
2025-04-09 07:03:10 +0000

    modified range

--- classes/importing_classes/car_backup.py
+++ classes/importing_classes/car_backup.py
@@ -2,13 +2,13 @@
 class Car:
     """A simple attempt to represent a car."""
 
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, year, range):
         """Initialize attributes to describe a car."""
         self.make = make
         self.model = model
         self.year = year
         self.odometer_reading = 0
-
+        self.range = range
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
         long_name = f"{self.year} {self.make} {self.model}"
@@ -19,14 +19,12 @@ class Car:
         print(f"This car has {self.odometer_reading} miles on it.")
 
     def update_odometer(self, mileage):
-        """
-        Set the odometer reading to the given value.
-        Reject the change if it attempts to roll the odometer back.
-        """
+        """Set the odometer reading to the given value."""
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
             print("You can't roll back an odometer!")
+
     def increment_odometer(self, miles):
         """Add the given amount to the odometer reading."""
         if miles >= 0:
@@ -51,14 +49,13 @@ class Battery:
             range = 260
         elif self.battery_size == 100:
             range = 315
-        else:
-            range = 240
         print(f"This car can go about {range} miles on a full charge.")
 
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, year, range):
         """Initialize attributes of the parent class."""
-        super().__init__(make, model, year)
+        super().__init__(make, model, year, range)
         self.battery = Battery()
+        self.battery.battery_size = range
