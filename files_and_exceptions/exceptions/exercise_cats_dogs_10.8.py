574843334113ef5f4a9fb452e9bc87c33ead7eeb
rupesh9999 <rupesh.b500@gmail.com>
2025-04-14 12:44:14 +0000

    created exercise 10.8

---
+++ files_and_exceptions/exceptions/exercise_cats_dogs_10.8.py
@@ -0,0 +1,14 @@
+def read_file(filename):
+    """Read the contents of a file and print them."""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"The file {filename} was not found.")
+    else:
+        print(f"Contents of {filename}:")
+        print(contents)
+
+filenames = ['cats.txt', 'dogs.txt']
+for filename in filenames:
+    read_file(filename)
