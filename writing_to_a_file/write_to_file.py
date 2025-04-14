2c1877d66d7527fe4320d5c33f5fda1d7440d964
rupesh9999 <rupesh.b500@gmail.com>
2025-04-14 05:53:24 +0000

    create_exercise_10.3

---
+++ writing_to_a_file/write_to_file.py
@@ -0,0 +1,4 @@
+with open('guest.txt', 'w') as file_object:
+    file_object.write('I love programming. \n')
+    file_object.write('I love creating new games. \n')
+    file_object.write('I love finding meaning in large datasets. \n')
