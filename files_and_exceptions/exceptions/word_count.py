84cd67e399344a371afcf77096cc3328458f75b5
rupesh9999 <rupesh.b500@gmail.com>
2025-04-14 11:27:15 +0000

    counting_words

--- files_and_exceptions/exceptions/word_count.py
+++ files_and_exceptions/exceptions/word_count.py
@@ -10,5 +10,6 @@ def count_words(filename):
         num_words = len(words)
         print(f"The file {filename} has about {num_words} words. ")
 
-filename = 'alice.txt'
-count_words(filename)
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
