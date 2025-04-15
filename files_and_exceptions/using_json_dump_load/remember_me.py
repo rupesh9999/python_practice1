459bf5652c3d4827ff0232ecc8d9a14440046ff5
rupesh9999 <rupesh.b500@gmail.com>
2025-04-15 10:30:31 +0000

    greet_user
    Ensures output ends with newline
    
    Ensures that the output in `remember_me.py` ends with a newline character. This improves file format consistency and is a standard practice for text files.

--- files_and_exceptions/using_json_dump_load/remember_me.py
+++ files_and_exceptions/using_json_dump_load/remember_me.py
@@ -4,4 +4,4 @@ username = input("What is your name?")
 filename = 'username.json'
 with open(filename, 'w') as f:
     json.dump(username, f)
-    print(f"We'll remember you when you come back, {username}!")
+    print(f"We'll remember you when you come back, {username}!")  
