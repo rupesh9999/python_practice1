1b7d652780e134a93967cf6d45f441dd02bb087a
rupesh9999 <rupesh.b500@gmail.com>
2025-04-15 12:24:33 +0000

    created_exercise_10.13

---
+++ files_and_exceptions/using_json_dump_load/verify_user.py
@@ -0,0 +1,36 @@
+import json
+
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'username5.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = 'username5.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        correct = input(f"Is {username} the correct username? (y/n) ")
+        if correct.lower() == 'y':
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username()
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
