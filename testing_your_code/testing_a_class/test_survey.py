81b3c01af971e4a2a68d7aed74d5ac7bd3486133
rupesh9999 <rupesh.b500@gmail.com>
2025-04-20 07:18:04 +0000

    run unit test

--- testing_your_code/testing_a_class/test_survey.py
+++ testing_your_code/testing_a_class/test_survey.py
@@ -12,5 +12,16 @@ class TestAnonymousSurvey(unittest.TestCase):
 
         self.assertIn('English', my_survey.responses)
 
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        question = "what language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+
 if __name__ == '__main__':
     unittest.main()
