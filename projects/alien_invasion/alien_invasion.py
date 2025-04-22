c43a28fc011af22bbe587957415440d601673ef0
rupesh9999 <rupesh.b500@gmail.com>
2025-04-22 07:32:57 +0000

    adding ship image

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
 
@@ -16,6 +18,8 @@ class AlienInvasion:
 #        self.screen = pygame.display.set_mode((1200, 800))
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
         # Set the background color.
         # self.bg_color = (230, 230, 230)
@@ -30,7 +34,9 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             #self.screen.fill(self.bg_color)
-            #self.screen.fill(self.settings.bg_color)       
+            #self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
             
             # Make the most recently drawn screen visible.
             pygame.display.flip()
