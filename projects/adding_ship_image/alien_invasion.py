52dbebfed04804c42f28632438bf13ec58fa06b5
rupesh9999 <rupesh.b500@gmail.com>
2025-04-23 06:03:52 +0000

    added ship imaged

--- projects/adding_ship_image/alien_invasion.py
+++ projects/adding_ship_image/alien_invasion.py
@@ -30,8 +30,8 @@ class AlienInvasion:
 
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blite()
+            #self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
