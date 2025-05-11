2d57e8e02b33274a1345f5e1228028ed6a789e52
rupesh9999 <rupesh.b500@gmail.com>
2025-05-11 11:02:18 +0000

    the_csv_file_format

---
+++ projects/chapter_16/the_csv_file_format/sitka_highs.py
@@ -0,0 +1,8 @@
+import csv
+filename = '/home/ubuntu/python_practice1/projects/chapter_16/the_csv_file_format/data/sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
